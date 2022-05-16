44f81a5d57ffe20db79d52bdc088a59fb1bb8975
Sheik Hazrin Bin Sheik Othman <sheikhazrin@outlook.my>
2022-05-16 13:09:14 +0800

    Using App Builder (Blazor & publich to Github

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using IgniteUI.Blazor.Controls;
+using UntitledApp;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+RegisterIgniteUI(builder.Services);
+
+await builder.Build().RunAsync();
+
+void RegisterIgniteUI(IServiceCollection services)
+{
+    services.AddIgniteUIBlazor(
+        typeof(IgbNavbarModule),
+        typeof(IgbButtonModule),
+        typeof(IgbIconModule),
+        typeof(IgbRippleModule),
+        typeof(IgbNavDrawerModule),
+        typeof(IgbNavDrawerItemModule)
+    );
+}
